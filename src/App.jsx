8ae7e6bd155c1e04bbf881505d63dd8da9d4c3a0
Satyendra Kumar <satyendrakumar8672461@gmail.com>
2023-10-11 14:57:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='w-full '>React router</h1>
+    </>
+  )
+}
+
+export default App
